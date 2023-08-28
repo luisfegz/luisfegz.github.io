3d612cce71c6f8195a1c52126b763962db0f5785
luisfegz <87459956+luisfegz@users.noreply.github.com>
2023-08-28 12:10:31 -0500

    Minute 50:23

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -2,19 +2,40 @@ import React from 'react';
 // link
 import { Link } from 'react-router-dom'
 const Product = ({ product }) => {
-  console.log(product)
+  // console.log(product);
   return (
     <Link>
-      <div>
+      <div className='grad w-full h-[362] rounded-[8px] overflow-hidden 
+      relative group'>
+        {/* badge */}
+        {product.attributes.isNew ? (
+          <div className='absolute bg-accent text-primary text-[12px]
+           font-extrabold uppercase top-4 right-4 px-2 rounded-full z-10'>
+            new
+          </div>
+        ) : (
+          ''
+        )}
         {/* image*/}
-        <div>
-          <img 
+        <div className='w-full h-[200px] flex items-center justify-center relative'>
+          <img
+            className='w-[160px] h-[160px] group-hover:scale-90 transition-all' 
             src={`http://localhost:1337${product.attributes.image.data.attributes.url}`} 
-            alt="" />
+            alt='' 
+          />
+        </div>
+        {/* text */}
+        <div>
+          {/* category title */}
+          <div>category title</div>
+          {/* title */}
+          <div>title</div>
+          {/* price */}
+          <div>price</div>
         </div>
       </div>
     </Link>
-  )
+  );
 };
 
 export default Product;
