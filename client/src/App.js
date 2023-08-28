fd6c83a62c9a3161485ce99612e37c2d6fd6f556
luisfegz <87459956+luisfegz@users.noreply.github.com>
2023-08-27 19:16:08 -0500

    Minute 13:31

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+
+// pages
+import Home from './pages/Home'
+import Products from './pages/Products'
+import ProductDetails from './pages/ProductDetails'
+import Search from './pages/Search'
+
+// Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+// Layout
+const Layout = () => {
+  return(
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+// Router
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [{path: '/', element: <Home /> },
+               {path: '/products/:id', element: <Products /> },
+               {path: '/product/:id', element: <ProductDetails /> },
+               {path: '/search', element: <Search /> },
+    ],
+  },
+]);
+
 
 const App = () => {
-  return <div>App</div>;
+  return <div>
+    <RouterProvider router={router}/>
+  </div>
 };
 
 export default App;
