8aea44dac3efecfaca169a8340c0ce88ccf285ad
luisfegz <87459956+luisfegz@users.noreply.github.com>
2023-08-28 10:05:18 -0500

    Minute 42:23

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
-const Product = () => {
-  return <div>Product</div>;
+// link
+import { Link } from 'react-router-dom'
+const Product = ({ product }) => {
+  console.log(product)
+  return (
+    <Link>
+      <div>
+        {/* image*/}
+        <div>
+          <img 
+            src={`http://localhost:1337${product.attributes.image.data.attributes.url}`} 
+            alt="" />
+        </div>
+      </div>
+    </Link>
+  )
 };
 
 export default Product;
--- client/src/components/ProductSlider.js
+++ client/src/components/ProductSlider.js
@@ -1,7 +1,29 @@
 import React from 'react';
+// import swiper react components
+import {Swiper, SwiperSlide} from 'swiper/react';
+ // import swiper styles
+ import 'swiper/css';
+ import 'swiper/css/pagination';
+ // impor required modules
+import { Pagination, Navigation} from 'swiper';
+// import components
+import Product from '../components/Product';
 
-const ProductSlider = () => {
-  return <div>ProductSlider</div>;
+
+const ProductSlider = ({ data }) => {
+  return (
+    <Swiper>
+      <>
+        {data?.map((product) => {
+          return (
+            <SwiperSlide key={product.id}>
+              <Product product={product}/>
+            </SwiperSlide>
+          );
+        })}
+      </>
+    </Swiper>
+  );
 };
 
 export default ProductSlider;
